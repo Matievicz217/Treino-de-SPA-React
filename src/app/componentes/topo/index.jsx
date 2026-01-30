ddc48e8d38b49cf7de5322af814db9f954979dd7
Matievicz217 <binhomatievicz@hotmail.com>
2026-01-30 16:06:37 -0300

    treino SPA React

---
+++ src/app/componentes/topo/index.jsx
@@ -0,0 +1,21 @@
+import style from "./topo.module.css";
+import Link from "next/link";
+
+export default function Topo() {
+  return (
+    <header className={style.container_topo}>
+      <h1>Navegação React</h1>
+      <nav>
+        <Link className={style.link_frontend} href="/frontend">
+          Front-end
+        </Link>
+        <Link className={style.link_backend} href="/backend">
+          Back-end
+        </Link>
+        <Link className={style.link_mobile} href="/mobile">
+          Mobile
+        </Link>
+      </nav>
+    </header>
+  );
+}
